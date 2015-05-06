605b5e18c702275495338e470e44f10ed09ae251
Graham Green <grahamgreen@gmail.com>
2015-05-06 09:37:16 -0500

    doc cleanup

--- doc.go
+++ doc.go
@@ -1,25 +1,29 @@
 //
 //agent_check opens a listen port that responds with the CPU idle % and a command port that accepts management commands
 //
-//Inspired by [this post](http://blog.loadbalancer.org/open-source-windows-service-for-reporting-server-load-back-to-haproxy-load-balancer-feedback-agent/)
+//Inspired by http://blog.loadbalancer.org/open-source-windows-service-for-reporting-server-load-back-to-haproxy-load-balancer-feedback-agent/
 //
 //Responds to telnet request with CPU Idle %.
+
 //Also opens up a command channel on localhost that will accept
+
 //management commands (i.e. Drain, Stop, Ready) Can be used with [HAProxy’s](http://www.haproxy.org/)
+
 //agent-check to weight traffic based on CPU load.
+
 //Note: The agent check functionality was added as of version 1.5
 //
-//[user@aServer ~]$ nc localhost 5309
-//UP 99%
-//[user@aServer ~]$ echo "DRAIN" | nc localhost 8675
-//DRAIN OK
-//[user@aServer ~]$ nc localhost 5309
-//DRAIN 99%
+//  [user@aServer ~]$ nc localhost 5309
+//  UP 99%
+//  [user@aServer ~]$ echo "DRAIN" | nc localhost 8675
+//  DRAIN OK
+//  [user@aServer ~]$ nc localhost 5309
+//  DRAIN 99%
 //
-//user@aServer ~]$ echo "READY" | nc localhost 8675
-//READY OK
-//[user@aServer ~]$ nc localhost 5309
-//READY 100%
+//  user@aServer ~]$ echo "READY" | nc localhost 8675
+//  READY OK
+//  [user@aServer ~]$ nc localhost 5309
+//  READY 100%
 //
 //agent_check can be run from the command line or with supervisord and supervisord-like utilities.
 package main
