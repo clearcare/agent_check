a48817d9379ad8ac2df6e885aa4076931fa4d582
Graham Green <grahamgreen@gmail.com>
2015-05-06 10:19:15 -0500

    more doc changes

--- doc.go
+++ doc.go
@@ -1,16 +1,13 @@
 //
-//agent_check opens a listen port that responds with the CPU idle % and a command port that accepts management commands
+//haproxy agent-check utility
 //
 //Inspired by http://blog.loadbalancer.org/open-source-windows-service-for-reporting-server-load-back-to-haproxy-load-balancer-feedback-agent/
 //
 //Responds to telnet request with CPU Idle %.
-
+//
 //Also opens up a command channel on localhost that will accept
-
 //management commands (i.e. Drain, Stop, Ready) Can be used with [HAProxy’s](http://www.haproxy.org/)
-
 //agent-check to weight traffic based on CPU load.
-
 //Note: The agent check functionality was added as of version 1.5
 //
 //  [user@aServer ~]$ nc localhost 5309
